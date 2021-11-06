4041f1d06879fe1b51f6f2b43b5866b72c25ddc0
Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>
2021-11-06 15:35:16 +0200

    docs(fish): update `check-port` fish function description
    This commit updates the `check-port` fish function description to use imperative
    present tense.

--- .config/fish/functions/check-port.fish
+++ .config/fish/functions/check-port.fish
@@ -1,7 +1,7 @@
 # ex. (check-port 8080)
 function check-port \
   --wraps=lsof \
-  --description='checks if any process occupied the specified port number' \
+  --description='Check if any process occupies the specified port number' \
   --argument-names port;
 
   sudo lsof -i :$port
