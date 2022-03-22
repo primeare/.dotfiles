3c74fbbf20bddfcb12d71336ee77491dbf6ffb6f
Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>
2022-03-22 16:14:59 +0200

    feat(fish config): vscode as default editor and env variables config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,12 @@
+# initialise Starship
 starship init fish | source
 
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-end
+# add global NPM package directory to PATH
+fish_add_path "~/.npm-packages/bin"
+
+# add Visual Studio Code (code) to PATH
+fish_add_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+# set Visual Studio Code as a default text editor
+set -gx EDITOR code --wait --new-window
+set -gx VISUAL code --wait --new-window
