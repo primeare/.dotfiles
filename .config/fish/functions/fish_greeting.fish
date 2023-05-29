90c35c89a4dfda5d5a9f0503a076f2ccbf9917cb
Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>
2023-05-29 17:35:20 +0300

    fix: improve fish_greeting message

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -2,16 +2,17 @@ function fish_greeting
   set --local current_hour (date +"%H")
 
   set_color $fish_color_user
-  if test $current_hour -ge 0 -a $current_hour -lt 12
+
+  if test $current_hour -ge 5 -a $current_hour -lt 12
     echo Good morning, $USER!
-  else if test $current_hour -ge 12 -a $current_hour -lt 17
+  else if test $current_hour -ge 12 -a $current_hour -lt 18
     echo Good afternoon, $USER!
-  else
+  else if test $current_hour -ge 18 -a $hour -lt 22
     echo Good evening, $USER!
+  else
+    echo Good night, $USER!
   end
+
   set_color normal
 
-  if type -q node
-    echo Node.js (node --version)
-  end
 end
