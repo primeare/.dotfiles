ac73fe3081c0b75014807fc8a04e88c5bd1151a4
Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>
2024-12-06 14:07:32 +0100

    feat: install vscode extensions from "extensions.json"
    Signed-off-by: Vladyslav Dukhin <15850680+primeare@users.noreply.github.com>

--- scripts/install-vscode-extensions.sh
+++ scripts/install-vscode-extensions.sh
@@ -3,31 +3,36 @@
 # exit immediately if a pipeline exits with a non-zero status
 set -Eeuxo pipefail
 
-"/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" \
-  --install-extension christian-kohler.npm-intellisense \
-  --install-extension christian-kohler.path-intellisense \
-  --install-extension cschlosser.doxdocgen \
-  --install-extension dbaeumer.vscode-eslint \
-  --install-extension dsznajder.es7-react-js-snippets \
-  --install-extension eamodio.gitlens \
-  --install-extension editorconfig.editorconfig \
-  --install-extension meganrogge.template-string-converter \
-  --install-extension mikestead.dotenv \
-  --install-extension mongodb.mongodb-vscode \
-  --install-extension ms-azuretools.vscode-docker \
-  --install-extension ms-vscode.hexeditor \
-  --install-extension ms-vscode.live-server \
-  --install-extension ms-vscode.vscode-js-profile-flame \
-  --install-extension prisma.prisma \
-  --install-extension rangav.vscode-thunder-client \
-  --install-extension redhat.vscode-commons \
-  --install-extension redhat.vscode-xml \
-  --install-extension redhat.vscode-yaml \
-  --install-extension sonarsource.sonarlint-vscode \
-  --install-extension streetsidesoftware.code-spell-checker \
-  --install-extension tamasfe.even-better-toml \
-  --install-extension timonwong.shellcheck \
-  --install-extension visualstudioexptteam.intellicode-api-usage-examples \
-  --install-extension visualstudioexptteam.vscodeintellicode \
-  --install-extension vscode-icons-team.vscode-icons \
-  --install-extension vsls-contrib.codetour
+VSCODE_EXTENSIONS_JSON_PATH="$(dirname $0)/../.vscode/extensions.json"
+VSCODE_CMD="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+
+if [[ ! -f "$VSCODE_EXTENSIONS_JSON_PATH" ]]; then
+  echo "Error: $VSCODE_EXTENSIONS_JSON_PATH file not found."
+  exit 1
+fi
+
+VSCODE_EXTENSIONS=$(node --input-type=module --eval "
+  import { readFileSync } from 'node:fs';
+
+  const data = readFileSync('$VSCODE_EXTENSIONS_JSON_PATH', 'utf8');
+  const json = JSON.parse(data);
+
+  if (!Array.isArray(json.recommendations)) {
+    throw new Error('\'recommendations\' is not an array.');
+  }
+
+  console.log(json.recommendations.join(' '));
+")
+
+if [[ -z "$VSCODE_EXTENSIONS" ]]; then
+  echo "No recommendations found in $VSCODE_EXTENSIONS_JSON_PATH."
+  exit 0
+fi
+
+INSTALL_FLAGS=()
+
+for EXTENSION in $VSCODE_EXTENSIONS; do
+  INSTALL_FLAGS+=(--install-extension "$EXTENSION")
+done
+
+"$VSCODE_CMD" "${INSTALL_FLAGS[@]}"
